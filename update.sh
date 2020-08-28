36a8d75d2c56311784348f4504e06f1a5de903f3
Florian Porada <florian.porada@gmail.com>
2020-08-28 16:04:15 +0200

    feat: remove orphan on startup

--- update.sh
+++ update.sh
@@ -5,10 +5,17 @@ set -e
 echo 'Enter infrastructure directory'
 cd /www/docker/server_infrastructure/
 
+# TODO: add dynamic docker-compose file builder
+# echo 'Stop container(s)'
+
+# echo 'Update container(s)'
+
+# echo 'Restart container(s)'
+
 echo 'Update docker-compose environment'
 docker-compose stop
 docker-compose rm -f
 docker-compose pull
-docker-compose up -d
+docker-compose up -d --remove-orphans
 
 echo 'Done!'
